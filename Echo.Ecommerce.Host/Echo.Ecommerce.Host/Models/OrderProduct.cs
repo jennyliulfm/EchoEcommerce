e1cdec990c23d934858feadd6bb60b07c37f2f70
jennyliulfm <chengyihang2008@gmail.com>
2020-08-06 21:20:22 +1000

    fix some problem in ordercontroller

--- Echo.Ecommerce.Host/Echo.Ecommerce.Host/Models/OrderProduct.cs
+++ Echo.Ecommerce.Host/Echo.Ecommerce.Host/Models/OrderProduct.cs
@@ -4,9 +4,9 @@ namespace Echo.Ecommerce.Host.Models
     public class OrderProduct
     {
         public int OrderProductId { get; set; }
-        public Order Order { get; set; }
+        //public Order Order { get; set; }
 
-        public Product Product { get; set; }
+        //public int ProductId { get; set; }
         public int Quantity { get; set; }
 
         public OrderProduct()
@@ -16,11 +16,10 @@ namespace Echo.Ecommerce.Host.Models
         public OrderProduct(Entities.OrderProduct model)
         {
             this.OrderProductId = model.OrderProductId;
-            this.Order = new Models.Order(model.Order);
-            this.Product = new Models.Product(model.Product);
+            this.OrderId = model.Order.OrderId;
+            this.ProductId = model.Product.ProductId;
             this.Quantity = model.Quantity;
         }
-
 
         public int OrderId { get; set; }
 
