5877349dfd3948edfb8fc6a61e27c2cb0b2ee911
jennyliulfm <chengyihang2008@gmail.com>
2020-08-11 17:27:23 +1000

    Finish Product Repository

---
+++ Echo.Ecommerce.Host/Echo.Ecommerce.Host/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Echo.Ecommerce.Host.Repositories
+{
+    public class IProductRepository
+    {
+        public IProductRepository()
+        {
+        }
+    }
+}
---
+++ Echo.Ecommerce.Host/Echo.Ecommerce.Host/Repositories/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Echo.Ecommerce.Host.Repositories
+{
+    public class ProductRepository
+    {
+        public ProductRepository()
+        {
+        }
+    }
+}
