29203ece2bcefb6ede9f196a7f4243c008cb7445
jennyliulfm <chengyihang2008@gmail.com>
2020-08-31 21:14:24 +1000

    add another banner

--- Echo.Ecommerce.Host/Echo-Ecommerce-Web/src/app/components/common/middlebanner/middlebanner.component.ts
+++ Echo.Ecommerce.Host/Echo-Ecommerce-Web/src/app/components/common/middlebanner/middlebanner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesEvent } from 'src/app/models/model';
 
 @Component({
   selector: 'app-middlebanner',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./middlebanner.component.css']
 })
 export class MiddlebannerComponent implements OnInit {
-
+  public salesEvents: Array<SalesEvent> =[];
   constructor() { }
 
   ngOnInit(): void {
+    this.getSalesEvents();
+  }
+
+  getSalesEvents() {
+    this.salesEvents = [
+      {
+        id:1,
+        name: "Mother's Day",
+        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",
+        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",       
+      },
+      {
+        id:3,
+        name: "Mother's Day",
+        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall2.png",
+        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall2.png",       
+      },
+    ];
   }
 
 }
