56eba9a951dabeab44afc81b6ba016a53c8e5a18
jennyliulfm <chengyihang2008@gmail.com>
2020-09-04 18:09:20 +1000

    change logo

--- Echo.Ecommerce.Host/Echo-Ecommerce-Web/src/app/components/common/middle-event/middle-event.component.ts
+++ Echo.Ecommerce.Host/Echo-Ecommerce-Web/src/app/components/common/middle-event/middle-event.component.ts
@@ -24,20 +24,20 @@ export class MiddleEventComponent implements OnInit {
       {
         id:1,
         name: "Mother's Day",
-        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",
-        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",       
+        photo_url: "../../../../assets/styles/images/banner/topbanner/1.png",
+        event_url: "../../../../assets/styles/images/banner/topbanner/1.png",       
       },
       {
         id:3,
         name: "Mother's Day",
-        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall2.png",
-        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall2.png",       
+        photo_url: "../../../../assets/styles/images/banner/topbanner/5.png",
+        event_url: "../../../../assets/styles/images/banner/topbanner/5.png",       
       },
       {
         id:3,
         name: "Mother's Day",
-        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",
-        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",     
+        photo_url: "../../../../assets/styles/images/banner/topbanner/6.png",
+        event_url: "../../../../assets/styles/images/banner/topbanner/6.png",     
       },
     ];
   }
--- Echo.Ecommerce.Host/Echo-Ecommerce-Web/src/app/components/common/middlebanner/middlebanner.component.ts
+++ Echo.Ecommerce.Host/Echo-Ecommerce-Web/src/app/components/common/middlebanner/middlebanner.component.ts
@@ -19,14 +19,14 @@ export class MiddlebannerComponent implements OnInit {
       {
         id:1,
         name: "Mother's Day",
-        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",
-        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall1.jpeg",       
+        photo_url: "../../../../assets/styles/images/banner/topbanner/1.png",
+        event_url: "../../../../assets/styles/images/banner/topbanner/1.png",       
       },
       {
         id:3,
         name: "Mother's Day",
-        photo_url: "../../../../assets/styles/images/banner/topbanner/rightsmall2.png",
-        event_url: "../../../../assets/styles/images/banner/topbanner/rightsmall2.png",       
+        photo_url: "../../../../assets/styles/images/banner/topbanner/5.png",
+        event_url: "../../../../assets/styles/images/banner/topbanner/5.png",       
       },
     ];
   }
